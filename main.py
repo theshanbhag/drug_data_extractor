07b2dc0e5858eb6df60c74f5fb6c2a4b75685f98
theshanbhag <venkatesh.shanbhag@mongodb.com>
2022-12-27 18:34:37 +0530

    updated

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from google.cloud import vision
+from google.cloud.vision_v1 import AnnotateImageResponse
+import json
 import pymongo
 import configparser
-from google.cloud import vision
-
 
 parser = configparser.ConfigParser()
 parser.read("config.txt")
 app = Flask(__name__)
 
-myclient = pymongo.MongoClient(parser.get("config", "uri"), tlsAllowInvalidCertificates=True)
-mydb = myclient["AppEngineTest"]
-mycol = mydb["Employee"]
-client = vision.ImageAnnotatorClient()
-
+mongo_client = pymongo.MongoClient(parser.get("config", "uri"), tlsAllowInvalidCertificates=True)
+mydb = mongo_client[parser.get("config", "database")]
+mycol = mydb[parser.get("config", "collection")]
+vision_client = vision.ImageAnnotatorClient()
 
 
 @app.route("/")
@@ -22,13 +22,26 @@ def homepage():
 
 @app.route("/submit", methods=['GET'])
 def docs():
+    # get file path from frontend
     args = request.args
     file_path = args['Bucket path']
+
+    # extract data using Google vision API
     blob_source = vision.Image(source=vision.ImageSource(image_uri=file_path))
-    results = client.text_detection(image=blob_source)
-    print(results)
-    print(">>>>>>>>>>>>>")
-    return "Done"
+    results = vision_client.text_detection(image=blob_source)
+
+    # Serialize the data and convert to json format
+    serialized_proto_plus = AnnotateImageResponse.serialize(results)
+    response = AnnotateImageResponse.deserialize(serialized_proto_plus)
+    response_json = AnnotateImageResponse.to_json(response)
+    response = json.loads(response_json)
+
+    try:
+        mycol.insert_one(response)
+    except Exception as e:
+        print(e)
+        return "Exception occurred :" + str(e)
+    return render_template("success.html")
 
 
 @app.route("/find", methods=['GET'])
