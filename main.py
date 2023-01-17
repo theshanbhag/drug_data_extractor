382932a2b5b0eac40e1c3e922267b6f348b4d1e2
Venkatesh Shanbhag <91714892+theshanbhag@users.noreply.github.com>
2023-01-17 19:48:15 +0530

    updated template

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from google.cloud import vision
 from google.cloud.vision_v1 import AnnotateImageResponse
+from google.cloud import storage
 import json
 import pymongo
 import configparser
@@ -24,34 +25,29 @@ def homepage():
 def docs():
     # get file path from frontend
     args = request.args
-    file_path = args['Bucket path']
-
-    # extract data using Google vision API
-    blob_source = vision.Image(source=vision.ImageSource(image_uri=file_path))
-    results = vision_client.text_detection(image=blob_source)
-
-    # Serialize the data and convert to json format
-    serialized_proto_plus = AnnotateImageResponse.serialize(results)
-    response = AnnotateImageResponse.deserialize(serialized_proto_plus)
-    response_json = AnnotateImageResponse.to_json(response)
-    response = json.loads(response_json)
-
+    bucket_name = args['Bucket name']
+    folder_name = args['Folder name']
+    storage_client = storage.Client()
+    bucket = storage.Bucket(storage_client, bucket_name)
     try:
-        mycol.insert_one(response)
+        blobs = bucket.list_blobs(prefix=folder_name)
+        for blob in blobs:
+            print(blob.name)
+            blob_source = vision.Image(
+                source=vision.ImageSource(image_uri="gs://" + str(bucket_name) + "/" +str(blob.name)))
+            results = vision_client.text_detection(image=blob_source)
+            serialized_proto_plus = AnnotateImageResponse.serialize(results)
+            response = AnnotateImageResponse.deserialize(serialized_proto_plus)
+            response_json = AnnotateImageResponse.to_json(response)
+            response = json.loads(response_json)
+            try:
+                mycol.insert_one(response["fullTextAnnotation"])
+            except Exception as e:
+                print(e)
     except Exception as e:
-        print(e)
-        return "Exception occurred :" + str(e)
+        print(str(e))
+
     return render_template("success.html")
-
-
-@app.route("/find", methods=['GET'])
-def find():
-    args = request.args
-    print("Hello World !!!!!")
-    x = mycol.find_one({"Name": args["find"]})
-    print(x.values())
-
-    return render_template("find.html", Names=x.items())
 
 
 if __name__ == "__main__":
